6f4bf3b410c04f9f42672310ac9824e0b25875df
jiangfly <487794648@qq.com>
2022-03-22 10:32:51 +0800

    demo的测试

--- Demo/src/main/java/com/example/demo/paid/controller/PaidController.java
+++ Demo/src/main/java/com/example/demo/paid/controller/PaidController.java
@@ -2,15 +2,13 @@ package com.example.demo.paid.controller;
 
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
-import com.example.demo.ins.entity.Enum.Status;
+import com.example.demo.dto.paidCondition;
 import com.example.demo.paid.entity.Paid;
 import com.example.demo.paid.service.IPaidService;
 import com.example.demo.tool.Baseseach.Basepage;
 import com.example.demo.tool.result.Result;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
-import org.springframework.stereotype.Controller;
 
 import javax.validation.constraints.NotNull;
 
@@ -22,7 +20,7 @@ import javax.validation.constraints.NotNull;
  * @author jiangFly
  * @since 2022-03-21
  */
-@Controller
+@RestController
 @RequestMapping("/paid")
 public class PaidController {
     @Autowired
@@ -33,8 +31,8 @@ public class PaidController {
         return Result.success(page);
     }
     @PostMapping
-    public Result condition(@RequestBody @NotNull Status status, Basepage basepage){
-        Basepage page = paidService.page(basepage, new QueryWrapper<Paid>().select("status", status.getValue()));
+    public Result condition(@RequestBody @NotNull paidCondition paidCondition){
+        Basepage page = paidService.page(paidCondition.getBasepage(), new QueryWrapper<Paid>().eq("status",paidCondition.getStatus()));
         return Result.success(page);
 
     }
@@ -46,6 +44,7 @@ public class PaidController {
     public Result addPaid(@RequestBody @NotNull Paid paid){
         return Result.success(paidService.save(paid));
     }
+
 
 
 }
